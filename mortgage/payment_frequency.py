4f1830c9ecf90098662af1a34ee4e2b899274fd3
Owen Maxwell <omaxwell@rrc.ca>
2025-07-25 11:28:48 -0500

    set up PaymentFrequency enumeration

--- mortgage/payment_frequency.py
+++ mortgage/payment_frequency.py
@@ -1,4 +1,12 @@
-
+__author__ = "Owen Maxwell"
+__version__ = "1.0.0"
 
 
 from enum import Enum
+
+class PaymentFrequency(Enum):
+    """Representing the options available for periods of time."""
+
+    MONTHLY: 12
+    BI_WEEKLY: 26
+    WEEKLY: 52
