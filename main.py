928f2827c65e9771824b7f78b5a35f17755fbe36
owen maxwell <omaxwell@rrc.ca>
2025-07-25 10:45:53 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+"""This module demonstrates your understanding of object-oriented
+programming.
+
+Below each of the comments, code the statement or statements to satisfy
+the requirement specified in the comment.
+"""
+
+__author__ = ""
+__version__ = ""
+__credits__ = "COMP-1327 Faculty"
+
+# 1. Create an instance of the Mortgage class. Initialize the object
+#    using values of your choosing. NOTE: This object will be used for 
+#    the following 4 questions.
+
+
+# 2. Print the official string representation of the object.
+
+
+# 3. Print the payment amount for the mortgage. Format the payment 
+#    amount as currency.
+
+
+# 4. Update the state of the object such that all attributes values are 
+#    different than what they were initialized to.
+
+
+# 5. Choose any attribute of the object and print it's current state.
+
+
+# 6. Attempt to create another instance of the Mortgage class. The 
+#    statement must use one value that will cause the initialization
+#    to fail. Prevent the script from abnormally ending and print the
+#    error message to the console.
